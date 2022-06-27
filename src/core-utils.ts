06b9691264fbaa101e175287371fe3be4c7fcb5b
George Cheng <Gerhut@GMail.com>
2022-06-27 12:27:36 +0800

    Update core-utils.ts

--- src/core-utils.ts
+++ src/core-utils.ts
@@ -11,7 +11,7 @@ export function omit<K extends string, T extends Record<K, unknown>>(object: T, 
 	return result
 }
 
-export function shortuuid(): string {
+export function shortuuid() {
 	let firstPart = (Math.random() * 46656) | 0;
 	let secondPart = (Math.random() * 46656) | 0;
 	return ("000" + firstPart.toString(36)).slice(-3) + ("000" + secondPart.toString(36)).slice(-3);
