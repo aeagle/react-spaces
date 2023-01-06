61f74583ffd95c361f95f55d818588390d7657cd
Allan Eagle <allan@allaneagle.com>
2023-01-06 22:59:02 +0000

    Revise demo

--- src/components/SSR.tsx
+++ src/components/SSR.tsx
@@ -2,7 +2,7 @@ import * as React from "react";
 import { enabledSsrSupport } from "../core-react";
 import css from "../styles.css";
 
-export const SSR: React.FC<{ children: React.ReactNode }> = ({ children }) => {
+export const SSR: React.FC<{ children?: React.ReactNode }> = ({ children }) => {
 	enabledSsrSupport();
 	return (
 		<>
