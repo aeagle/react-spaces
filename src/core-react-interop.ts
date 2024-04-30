f2ac626d42e5d754f0ba5288d57a0ebc02399cc4
Allan Eagle <allan@allaneagle.com>
2024-04-30 22:35:39 +0100

    Another attempt

--- src/core-react-interop.ts
+++ src/core-react-interop.ts
@@ -1,19 +1,20 @@
 import { shortuuid } from "./core-utils";
 const React = require("react");
 
-export function useUniqueId(ssrEnabled: boolean) {
-	if (ssrEnabled) {
-		if (React.version.startsWith("18") && typeof React.useId !== "undefined") {
-			return `s${React.useId().replace(/\:/g, "")}`;
-		}
-
-		// @ts-ignore
-		if (typeof React.unstable_useOpaqueIdentifier !== "undefined") {
-			console.log("React 17 legacy");
-			// @ts-ignore
-			return `s${React.unstable_useOpaqueIdentifier().replace(/\:/g, "")}`;
-		}
+function getIdFn() {
+	if (React.version.startsWith("18") && typeof React.useId !== "undefined") {
+		return React.useId;
 	}
+	// @ts-ignore
+	if (typeof React.unstable_useOpaqueIdentifier !== "undefined") {
+		console.log("React 17 legacy");
+		// @ts-ignore
+		return React.unstable_useOpaqueIdentifier;
+	}
+	return () => `s${shortuuid()}`;
+}
 
-	return `s${shortuuid()}`;
+export function useUniqueId(ssrEnabled: boolean) {
+	const idFn = getIdFn();
+	return `s${idFn().replace(/\:/g, "")}`;
 }
